c22180e8e7534f7abc7f963c5e4fac8100fd051d
leehsanttana <leonamsanttana@gmail.com>
2021-12-09 22:30:09 -0300

    tentando corrigir um bug no qual os estilos dos modais não eram especificados no modo mobile

--- src/Components/Loading/index.js
+++ src/Components/Loading/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className="loading">
-      <img src="https://pngimage.net/electronics-images-png/" alt="caregando" />
+      {/* <img src="https://pngimage.net/electronics-images-png/" alt="caregando" /> */}
       <h1>
         Carregando <span className="dot">.</span>{' '}
       </h1>
